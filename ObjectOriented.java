8fbcfff091b1292b17d0aa755f93577e6ea9210d
ashishranjan2011 <ashishranjan@iitg.ac.in>
2020-07-12 15:26:52 +0530

    Feature added: Added Wheel Class 	Sepration of data from gear class to make wheel class as new functionalities were breaking single responsibility

--- ObjectOriented.java
+++ ObjectOriented.java
@@ -1,23 +1,39 @@
 package learnOOD;
+class Wheel{
+	private int rim;
+	private int tire;
+	public Wheel(int rim,int tire){
+		this.rim=rim;
+		this.tire=tire;
+	}
+	public int diameter(){
+		return rim+2*tire;
+	}
+	public double circumference(){
+		return diameter()*3.14;
+	}
+}
 class Gear{
 	private int chainring;
 	private int cog;
-	private int rim;
-	private int tire;
-	public Gear(int chainring,int cog,int rim,int tire){
+	private Wheel wheel;
+	public Gear(int chainring,int cog,Wheel wheel){
 		this.chainring=chainring;
 		this.cog=cog;
-		this.rim=rim;
-		this.tire=tire;
+		this.wheel=wheel;
 	}
 	public double ratio(){
 		return (double)this.chainring/(double)this.cog;
 	}
+	public double gear_inches(){
+		return ratio()*wheel.diameter();
+	}
 }
 class ObjectOriented{
 	public static void main(String[] args){
-		Gear gear=new Gear(12,7,6,56);
+		Wheel wheel=new Wheel(26,2);
+		Gear gear=new Gear(12,7,wheel);
 		System.out.println(gear.ratio());
-
+		System.out.println(gear.gear_inches());
 	}
-} 
+}
